6d0f08fcdd335964a7d9c361ba2a6cf8697f3665
user <user@DESKTOP-SLUGB16>
2020-08-24 09:20:01 +0500

    Add project files.

---
+++ ContosoCraft/Controller/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoCraft.Model;
+using ContosoCraft.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoCraft.Controller
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductService _productService { get; }
+        public ProductsController(ProductService productService )
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return _productService.GetProducts();
+        }
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get([FromQuery] string productId,[FromQuery] int rating)
+        {
+            _productService.AddRating(productId,rating);
+            return Ok();
+        }
+    }
+}
---
+++ ContosoCraft/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoCraft.Model;
+using ContosoCraft.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCraft.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public ProductService _ProductService;
+        public IEnumerable<Product> products;
+        public IndexModel(ILogger<IndexModel> logger , ProductService productService)
+        {
+            _logger = logger;
+            _ProductService = productService;
+        }
+
+        public void OnGet()
+        {
+            products = _ProductService.GetProducts();
+        }
+    }
+}
